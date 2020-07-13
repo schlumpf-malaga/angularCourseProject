d74309d456405b8c8b2562f4369fbdfd01322b97
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-07-13 08:45:22 +0200

    feat: header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ScheduleComponent } from './areas/external/views/schedule/schedule.component';
 import { WeeklyScreeningsComponent } from './shared/components/weekly-screenings/weekly-screenings.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
-	declarations: [AppComponent, ScheduleComponent, WeeklyScreeningsComponent],
+	declarations: [AppComponent, ScheduleComponent, WeeklyScreeningsComponent, HeaderComponent, FooterComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
