4311db8515712ce0d9fdd8f0636ff043d366acda
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-07-15 12:24:55 +0200

    router implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeaderComponent } from './shared/components/header/header.component';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import localeDeExtra from '@angular/common/locales/extra/de';
@@ -9,9 +10,10 @@ import { MovieCardComponent } from '@shared/components/movie-card/movie-card.com
 import { MovieSummaryLineComponent } from '@shared/components/movie-summary-line/movie-summary-line.component';
 import { WeeklyScreeningsComponent } from '@shared/components/weekly-screenings/weekly-screenings.component';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { LocationComponent } from './areas/external/views/location/location.component';
-import { ScheduleComponent } from './areas/external/views/schedule/schedule.component';
 import { LoginComponent } from './areas/external/views/login/login.component';
+import { ScheduleComponent } from './areas/external/views/schedule/schedule.component';
 import { SeatSelectionComponent } from './areas/external/views/seat-selection/seat-selection.component';
 import { SelectionsComponent } from './areas/external/views/selections/selections.component';
 import { ReservedSeatsComponent } from './areas/internal/views/reserved-seats/reserved-seats.component';
@@ -21,18 +23,19 @@ registerLocaleData(localeDe, localeDeExtra);
 @NgModule({
 	declarations: [
 		AppComponent,
-		ScheduleComponent,
-		WeeklyScreeningsComponent,
 		FooterComponent,
-		MovieSummaryLineComponent,
-		MovieCardComponent,
+		HeaderComponent,
 		LocationComponent,
 		LoginComponent,
+		MovieCardComponent,
+		MovieSummaryLineComponent,
+		ReservedSeatsComponent,
+		ScheduleComponent,
 		SeatSelectionComponent,
 		SelectionsComponent,
-		ReservedSeatsComponent,
+		WeeklyScreeningsComponent,
 	],
-	imports: [BrowserModule],
+	imports: [BrowserModule, AppRoutingModule],
 	providers: [...stubServiceProviders, { provide: LOCALE_ID, useValue: 'de' }],
 	bootstrap: [AppComponent],
 })
--- src/app/shared/components/movie-summary-line/movie-summary-line.component.spec.ts
+++ src/app/shared/components/movie-summary-line/movie-summary-line.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MovieSummaryLineComponent } from './movie-summary-line.component';
 
 describe('MovieSummaryLineComponent', () => {
-  let component: MovieSummaryLineComponent;
-  let fixture: ComponentFixture<MovieSummaryLineComponent>;
+	let component: MovieSummaryLineComponent;
+	let fixture: ComponentFixture<MovieSummaryLineComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MovieSummaryLineComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [MovieSummaryLineComponent],
+		}).compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MovieSummaryLineComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MovieSummaryLineComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
