be75ae207b7313e7e1b052aaae4fd082fd095f46
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-07-15 09:02:05 +0200

    header no longer a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { ScheduleComponent } from './areas/external/views/schedule/schedule.component';
 import { FooterComponent } from '@shared/components/footer/footer.component';
-import { HeaderComponent } from '@shared/components/header/header.component';
 import { MovieCardComponent } from '@shared/components/movie-card/movie-card.component';
 import { MovieSummaryLineComponent } from '@shared/components/movie-summary-line/movie-summary-line.component';
 import { WeeklyScreeningsComponent } from '@shared/components/weekly-screenings/weekly-screenings.component';
+import { AppComponent } from './app.component';
+import { ScheduleComponent } from './areas/external/views/schedule/schedule.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		ScheduleComponent,
 		WeeklyScreeningsComponent,
-		HeaderComponent,
 		FooterComponent,
 		MovieSummaryLineComponent,
 		MovieCardComponent,
