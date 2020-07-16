17e823a55fdf9c4020b6056a6cb6161b4a67c1e2
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-07-16 17:56:31 +0200

    fix: footer-height

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
@@ -7,5 +7,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-	currentComponent = 'shedule';
+	@HostBinding() class = 'app-root';
 }
