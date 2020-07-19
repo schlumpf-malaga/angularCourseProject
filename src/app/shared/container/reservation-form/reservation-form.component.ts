89c8f5c9c80b60ba943af3b2abfb5f9073b7ebea
Krzysztof Franek <krzysztof.franek@metafinanz.de>
2020-07-19 19:57:46 +0200

    finished

--- src/app/shared/container/reservation-form/reservation-form.component.ts
+++ src/app/shared/container/reservation-form/reservation-form.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { onSubmit_reservationForm } from '@core/events/onSubmit_reservationForm';
 
 @Component({
@@ -8,5 +9,21 @@ import { onSubmit_reservationForm } from '@core/events/onSubmit_reservationForm'
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReservationFormComponent {
+	formGroup = new FormGroup({
+		firstName: new FormControl(''),
+		lastName: new FormControl(''),
+		email: new FormControl('', [Validators.email, Validators.required]),
+	});
+
 	@Output() formSubmit = new EventEmitter<onSubmit_reservationForm>();
+
+	onSubmit_form() {
+		if (this.formGroup.valid) {
+			this.formSubmit.next({
+				firstName: this.formGroup.get('firstName').value,
+				lastName: this.formGroup.get('lastName').value,
+				email: this.formGroup.get('email').value,
+			});
+		}
+	}
 }
--- src/app/shared/container/reservation-form/reservation-form.module.ts
+++ src/app/shared/container/reservation-form/reservation-form.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ReservationFormComponent } from './reservation-form.component';
 
 @NgModule({
 	declarations: [ReservationFormComponent],
-	imports: [CommonModule],
+	imports: [CommonModule, ReactiveFormsModule],
 	exports: [ReservationFormComponent],
 })
 export class AppReservationFormModule {}
